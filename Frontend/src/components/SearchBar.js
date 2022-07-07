6e3a7ea6ca4348dbe4a8891710a5bd7a7e18db0f
Kasia Laniecka <kl@parkshare.dk>
2022-07-07 15:31:03 -0400

    update styling of filters on main

--- Frontend/src/components/SearchBar.js
+++ Frontend/src/components/SearchBar.js
@@ -1,48 +1,52 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 // import './App.css';
 // import item from '../assets/data/items'
-import { Searchbar } from 'react-native-paper';
+import { Searchbar } from "react-native-paper";
 import { StyleSheet, Button, View, TextInput } from "react-native";
-import Card from './Card';
-import Item from '../assets/data/items'
+import Card from "./Card";
+import Item from "../assets/data/items";
+import TextButton from "./TextButton";
 
+export default function Searching({ item, setItem, filterSearchItem }) {
+  const [searchTerm, setSearchTerm] = useState("");
 
-export default function Searching({item , setItem , filterSearchItem}) {
+  return (
+    <>
+      <View>
+        {/*<TextInput*/}
+        {/*  style={styles.searchBar}*/}
+        {/*  onChangeText={(e) => {*/}
+        {/*    setSearchTerm(e);*/}
+        {/*    filterSearchItem(e, setSearchTerm);*/}
+        {/*  }}*/}
 
-    const [searchTerm,setSearchTerm] = useState('')
+        {/*/>*/}
+        <Searchbar
+          placeholder="Search"
+          onChangeText={(e) => {
+            setSearchTerm(e);
+            filterSearchItem(e, setSearchTerm);
+          }}
+          value={searchTerm}
+          style={styles.searchBar}
+        />
 
-
-    return (
-      <>
-        <View>
-          {/*<TextInput*/}
-          {/*  style={styles.searchBar}*/}
-          {/*  onChangeText={(e) => {*/}
-          {/*    setSearchTerm(e);*/}
-          {/*    filterSearchItem(e, setSearchTerm);*/}
-          {/*  }}*/}
-
-          {/*/>*/}
-            <Searchbar
-                placeholder="Search"
-                onChangeText={(e) => {
-                    setSearchTerm(e);
-                    filterSearchItem(e, setSearchTerm)}}
-                value={searchTerm}
-                style={styles.searchBar}
-            />
-
-          <Button title="Clear" onPress={() => setItem(Item)} />
-        </View>
-      </>
-    );
+        <TextButton
+          title="Clear"
+          onPress={() => setItem(Item)}
+          style={styles.clear}
+        />
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
   searchBar: {
-    margin: 20,
     borderColor: "black",
-
+    marginBottom: 20,
+  },
+  clear: {
+    alignSelf: "center",
   },
 });
-
