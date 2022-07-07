1e002a2390f16a29d5ccd398903879af9a9e4aaf
Kasia Laniecka <kl@parkshare.dk>
2022-07-07 14:54:11 -0400

    navigation

--- Frontend/src/navigations/MainPageNavigation.js
+++ Frontend/src/navigations/MainPageNavigation.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { createStackNavigator } from '@react-navigation/stack';
-import MainPage from '../screens/MainPage';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from "@react-navigation/stack";
+import MainPage from "../screens/MainPage";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AddProductScreenNavigation from "./AddProductScreenNavigation";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import ProfileScreenNavigation from "./ProfileScreenNavigation";
 import ProductDetailsScreen from "../screens/ProductDetailsScreen";
 import ProfileScreen from "../screens/ProfileScreen";
@@ -12,52 +12,73 @@ const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function Home() {
-    return (
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home" component={MainPage} options={{
-          tabBarLabel: 'Home',
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { height: 90, padding: 8 },
+        tabBarActiveTintColor: "#2E3C6F",
+        tabBarIconStyle: { height: 10 },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={MainPage}
+        options={{
+          tabBarLabel: "Home",
+          tabBarLabelStyle: { marginTop: 4 },
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={40} />
+            <MaterialCommunityIcons name="home" color={color} size={35} />
           ),
-        }}/>
-        <Tab.Screen name="add" component={AddProductScreenNavigation} options={{
-          tabBarLabel: 'Sell',
+        }}
+      />
+      <Tab.Screen
+        name="add"
+        component={AddProductScreenNavigation}
+        options={{
+          tabBarLabel: "Sell",
+          tabBarLabelStyle: { marginTop: 4 },
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="plus-circle-outline" color={color} size={40} />
+            <MaterialCommunityIcons
+              name="plus-circle-outline"
+              color={color}
+              size={35}
+            />
           ),
-        }} />
-        <Tab.Screen name="Profile" component={ProfileScreenNavigation}  options={{
-          tabBarLabel: 'Profile',
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreenNavigation}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarLabelStyle: { marginTop: 4 },
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account" color={color} size={40} />
+            <MaterialCommunityIcons name="account" color={color} size={35} />
           ),
-        }} />
-      </Tab.Navigator>
-    );
-  }
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 const MainScreenNavigation = () => {
-    return( 
+  return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-        
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="main" component={MainScreenNavigation} />
-         <Stack.Screen 
-            name='productDetails'
-            component={ProductDetailsScreen}
-            options={{
-                title: 'Details',
-                headerShown: true,
-            }}
-            />    
-        <Stack.Screen name="+" component={AddProductScreenNavigation} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
- 
-       
-    </Stack.Navigator>    
-     
-    
-    )
-}
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="main" component={MainScreenNavigation} />
+      <Stack.Screen
+        name="productDetails"
+        component={ProductDetailsScreen}
+        options={{
+          title: "Details",
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen name="+" component={AddProductScreenNavigation} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+    </Stack.Navigator>
+  );
+};
 
 export default MainScreenNavigation;
